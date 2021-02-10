da94cbf483cc4001203e5a9883c90b437cfb9616
ShakhSaidov <www.blackbeard963@gmail.com>
2021-02-10 14:44:35 -0500

    Added the 'next' function as a parameter to handle errors

--- Backend/src/routes/products.js
+++ Backend/src/routes/products.js
@@ -1,30 +1,36 @@
 import express from 'express'
 import productsService from "../services/productsService.js"
-import logger from "../../utils/logger.js"
 const router = express.Router()
 
 //GET request for all products from product list
 router.get('/', (request, response) => {
-  const products = productsService.getAllProducts()
-  response.json(products)
+  try {
+    const products = productsService.getAllProducts()
+    response.json(products)
+  } catch (error) {
+    next(error)
+  }
 })
 
 //GET request for a specific product
-router.get('/:id', (request, response) => {
-  const product = productsService.findProductByID(request.params.id)
-  if (product) {
-    return response.json(product)
-  } else {
-    return response.status(400).send({ error: "invalid id" })
+router.get('/:id', (request, response, next) => {
+  try {
+    const product = productsService.findProductByID(request.params.id)
+    if (product) {
+      return response.json(product)
+    } else {
+      return response.status(400).send({ error: "invalid id" })
+    }
+  } catch (error) {
+    next(error)
   }
 })
 
 //POST request to add a new product onto the product list
-router.post('/', (request, response) => {
+router.post('/', (request, response, next) => {
   try {
     const { error } = productsService.validateProduct(request.body)
     if (error) {
-      logger.log("Error from Joi: ", error);
       return response.status(400).json({
         error: error.details.map(detail => detail.message)
       })
@@ -32,19 +38,18 @@ router.post('/', (request, response) => {
       const newProduct = productsService.addProduct(request.body)
       response.json(newProduct)
     }
-  } catch (e) {
-    logger.log("Error from catch: ", e);
-    response.status(400).json(e.message)
+  } catch (error) {
+    next(error)
   }
 })
 
 //DELETE request to remove a product form product list
-router.delete('/:id', (request, response) => {
+router.delete('/:id', (request, response, next) => {
   try {
     productsService.deleteProduct(request.params.id)
     response.status(204).end()
-  } catch (e) {
-    response.status(400).json(e.message)
+  } catch (error) {
+    next(error)
   }
 })
 
