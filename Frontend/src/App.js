eb7aedbd041e165f6a6251314b94b4e5aa44a910
ShakhSaidov <www.blackbeard963@gmail.com>
2021-02-11 16:42:32 -0500

    Etag method seemed convoluted, will use socket.io

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,75 +7,75 @@ import Switch from './components/Switch'
 import './App.css'
 
 const App = () => {
-  const [products, setProducts] = useState([])
-  const [message, setMessage] = useState(null)
-  const productFormRef = useRef()
+    const [products, setProducts] = useState([])
+    const [message, setMessage] = useState(null)
+    const productFormRef = useRef()
 
-  useEffect(() => {
-    serverRoutes
-      .getList()
-      .then(initialProducts => setProducts(initialProducts))
-  }, [])
+    useEffect(() => {
+        serverRoutes
+            .getList()
+            .then(initialProducts => setProducts(initialProducts))
+    }, [])
 
-  console.log("Products is: ", products);
-  console.log("Products length is: ", products.length);
+    console.log("Products is: ", products);
+    console.log("Products length is: ", products.length);
 
-  const handleAdd = newProduct => {
-    const newName = newProduct.name;
-    if (!products.find(product => product.name === newName)) {
-      serverRoutes
-        .add(newProduct)
-        .then(returnedNewProduct => {
-          setProducts(products.concat(returnedNewProduct))
-        })
-        .catch(e => console.log(e))
+    const handleAdd = newProduct => {
+        const newName = newProduct.name;
+        if (!products.find(product => product.name === newName)) {
+            serverRoutes
+                .add(newProduct)
+                .then(returnedNewProduct => {
+                    setProducts(products.concat(returnedNewProduct))
+                })
+                .catch(e => console.log(e))
 
-    } else {
-      setMessage("Product name already exists in the list!")
-      setTimeout(() => {
-        setMessage(null)
-      }, 5000)
+        } else {
+            setMessage("Product name already exists in the list!")
+            setTimeout(() => {
+                setMessage(null)
+            }, 5000)
+        }
+        productFormRef.current.switchButton()
     }
-    productFormRef.current.switchButton()
-  }
 
-  const handleRemove = (event, id, number) => {
-    event.preventDefault()
+    const handleRemove = (event, id, number) => {
+        event.preventDefault()
 
-    if (window.confirm(`Are you sure you want to remove Product ${number}?`)) {
-      serverRoutes
-        .remove(id)
-        .then(setProducts(products.filter(product => product.id !== id)))
+        if (window.confirm(`Are you sure you want to remove Product ${number}?`)) {
+            serverRoutes
+                .remove(id)
+                .then(setProducts(products.filter(product => product.id !== id)))
+        }
     }
-  }
 
-  return (
-    <div>
-      <meta
-        name="viewport"
-        content="minimum-scale=1, initial-scale=1, width=device-width"
-      />
-      <div className="center">
-        <Message
-          message={message}
-          empty={products.length === 0 ? true : false}/>
-      </div>
-      {products.length !== 0 && <h1 className="center">Products List</h1>}
-      <Switch buttonLabel="Add new product" ref={productFormRef}>
-        <NewProductForm handleAdd={handleAdd} />
-      </Switch>
-      <div>
-        {products.map((product, index) =>
-          <Product
-            key={product.id}
-            product={product}
-            number={index + 1}
-            handleRemove={handleRemove}
-          />
-        )}
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <meta
+                name="viewport"
+                content="minimum-scale=1, initial-scale=1, width=device-width"
+            />
+            <div className="center">
+                <Message
+                    message={message}
+                    empty={products.length === 0 ? true : false} />
+            </div>
+            {products.length !== 0 && <h1 className="center">Products List</h1>}
+            <Switch buttonLabel="Add new product" ref={productFormRef}>
+                <NewProductForm handleAdd={handleAdd} />
+            </Switch>
+            <div>
+                {products.map((product, index) =>
+                    <Product
+                        key={product.id}
+                        product={product}
+                        number={index + 1}
+                        handleRemove={handleRemove}
+                    />
+                )}
+            </div>
+        </div>
+    )
 }
 
 export default App;
--- Frontend/src/server/ProductsList.js
+++ Frontend/src/server/ProductsList.js
@@ -2,24 +2,24 @@ import axios from 'axios'
 const rootUrl = '/products'
 
 const getList = () => {
-  const request = axios.get(rootUrl)
-  return request.then(response => response.data)
+    const request = axios.get(rootUrl)
+    return request.then(response => response.data)
 }
 
 const add = newProduct => {
-  const request = axios.post(rootUrl, newProduct)
-  return request.then(response => response.data)
+    const request = axios.post(rootUrl, newProduct)
+    return request.then(response => response.data)
 }
 
 const remove = id => {
-  const request = axios.delete(`${rootUrl}/${id}`)
-  return request.then(response => response.data)
+    const request = axios.delete(`${rootUrl}/${id}`)
+    return request.then(response => response.data)
 }
 
 const serverRoutes = {
-  getList,
-  add,
-  remove
+    getList,
+    add,
+    remove
 }
 
 export default serverRoutes
