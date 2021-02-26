64cf81025c5dcbb7654e00b6af23133968435467
ShakhSaidov <www.blackbeard963@gmail.com>
2021-02-26 10:42:52 -0500

    Refactored and working

--- Backend/data/productsData.js
+++ Backend/data/productsData.js
@@ -17,7 +17,262 @@ const products = {
         name: "FORGE",
         description: "The world's first sport-focused publishing platform.",
         quantity: 1
-    }
+    },
+
+    "JeC5pqwepJw2YZnV8ot1m":
+    {
+        name: "Data Services",
+        description: "Our advanced, real-time data tracking solutions empower rights holders to maximize the value of their content",
+        quantity: 99
+    },
+
+    "JeC5pzewtwtwetewttt1m":
+    {
+        name: "Design Services",
+        description: "Our Design Services help sports operators clarify their vision, investigate user needs, design a powerful digital product, and iterate on the results",
+        quantity: 75
+    },
+
+    "qwetfdbfdbpojgadigdgj":
+    {
+        name: "VISUAL STORIES",
+        description: "This flexible module enables operators to craft bite-sized messages pre, during, and post-game to make your digital experiences even more compelling.",
+        quantity: 44
+    },
+
+    "xvmnkjhioeurwghbbfbff":
+    {
+        name: "LIVE BLOGGING",
+        description: "This flexible module enables operators to craft bite-sized messages pre, during, and post-game to make your digital experiences even more compelling.",
+        quantity: 10
+    },
+
+    "zcxbzcxbmnkjiCOLwetew":
+    {
+        name: "mtribes",
+        description: "mtribes is a SaaS platform for real-time, data-driven UX targeting",
+        quantity: 51
+    },
+
+    "w11H5lBZ2UPC5P6Hi9Rzp": {
+        name: "DIVA",
+        description: "An advanced OTT player, synchronizing real-time data with rich interactivity.",
+        quantity: 15,
+    },
+
+    "SY4pBCLf6ql4nl6r5m_E7":
+    {
+        name: "AXIS",
+        description: "A targeted UX management console and suite of multi-platform reference apps.",
+        quantity: 100
+    },
+
+    "JeC5pzjspvw2YZnV8ot1m":
+    {
+        name: "FORGE",
+        description: "The world's first sport-focused publishing platform.",
+        quantity: 1
+    },
+
+    "JeCtpqwepJw2YZnV8ot1m":
+    {
+        name: "Data Services",
+        description: "Our advanced, real-time data tracking solutions empower rights holders to maximize the value of their content",
+        quantity: 99
+    },
+
+    "JeC5pzewgdtwetewttt1m":
+    {
+        name: "Design Services",
+        description: "Our Design Services help sports operators clarify their vision, investigate user needs, design a powerful digital product, and iterate on the results",
+        quantity: 75
+    },
+
+    "qwetfdbfdbpojgadiwrgj":
+    {
+        name: "VISUAL STORIES",
+        description: "This flexible module enables operators to craft bite-sized messages pre, during, and post-game to make your digital experiences even more compelling.",
+        quantity: 44
+    },
+
+    "xvmnklpioeurwghbbfbff":
+    {
+        name: "LIVE BLOGGING",
+        description: "This flexible module enables operators to craft bite-sized messages pre, during, and post-game to make your digital experiences even more compelling.",
+        quantity: 10
+    },
+
+    "zcxbzcxbmbkjiCOLwetew":
+    {
+        name: "mtribes",
+        description: "mtribes is a SaaS platform for real-time, data-driven UX targeting",
+        quantity: 51
+    },
+
+    "w11H5lBZ2UtT5P6Hi9RzT": {
+        name: "DIVA",
+        description: "An advanced OTT player, synchronizing real-time data with rich interactivity.",
+        quantity: 15,
+    },
+
+    "SY4pbvLf6ql4nl6E5m_E7":
+    {
+        name: "AXIS",
+        description: "A targeted UX management console and suite of multi-platform reference apps.",
+        quantity: 100
+    },
+
+    "JeC5pzjspow2YZnV8ot1m":
+    {
+        name: "FORGE",
+        description: "The world's first sport-focused publishing platform.",
+        quantity: 1
+    },
+
+    "JeC5erwepJw2YZnV8ot1m":
+    {
+        name: "Data Services",
+        description: "Our advanced, real-time data tracking solutions empower rights holders to maximize the value of their content",
+        quantity: 99
+    },
+
+    "JeC5pzewbvtwetewttt1m":
+    {
+        name: "Design Services",
+        description: "Our Design Services help sports operators clarify their vision, investigate user needs, design a powerful digital product, and iterate on the results",
+        quantity: 75
+    },
+
+    "qwetfdbbvcpojgadigdgj":
+    {
+        name: "VISUAL STORIES",
+        description: "This flexible module enables operators to craft bite-sized messages pre, during, and post-game to make your digital experiences even more compelling.",
+        quantity: 44
+    },
+
+    "trmnkjhioeurwghbbfbff":
+    {
+        name: "LIVE BLOGGING",
+        description: "This flexible module enables operators to craft bite-sized messages pre, during, and post-game to make your digital experiences even more compelling.",
+        quantity: 10
+    },
+
+    "zcxbzcxbmnkjiCOLwepol":
+    {
+        name: "mtribes",
+        description: "mtribes is a SaaS platform for real-time, data-driven UX targeting",
+        quantity: 51
+    },
+
+    "w11H5lBZRHPC5P6Hi9RzT": {
+        name: "DIVA",
+        description: "An advanced OTT player, synchronizing real-time data with rich interactivity.",
+        quantity: 15,
+    },
+
+    "SY4pBGCf6ql4nl6E5m_E7":
+    {
+        name: "AXIS",
+        description: "A targeted UX management console and suite of multi-platform reference apps.",
+        quantity: 100
+    },
+
+    "JeC5pzjspTR2YZnV8ot1m":
+    {
+        name: "FORGE",
+        description: "The world's first sport-focused publishing platform.",
+        quantity: 1
+    },
+
+    "JeCYG9wepJw2YZnV8ot1m":
+    {
+        name: "Data Services",
+        description: "Our advanced, real-time data tracking solutions empower rights holders to maximize the value of their content",
+        quantity: 99
+    },
+
+    "JeC5pzewtw653tewttt1m":
+    {
+        name: "Design Services",
+        description: "Our Design Services help sports operators clarify their vision, investigate user needs, design a powerful digital product, and iterate on the results",
+        quantity: 75
+    },
+
+    "qwetfdbfdb4H2gadigdgj":
+    {
+        name: "VISUAL STORIES",
+        description: "This flexible module enables operators to craft bite-sized messages pre, during, and post-game to make your digital experiences even more compelling.",
+        quantity: 44
+    },
+
+    "xvmnkjhioeuyrghbbfbff":
+    {
+        name: "LIVE BLOGGING",
+        description: "This flexible module enables operators to craft bite-sized messages pre, during, and post-game to make your digital experiences even more compelling.",
+        quantity: 10
+    },
+
+    "zcxb9kxbmnkjiCOLwetew":
+    {
+        name: "mtribes",
+        description: "mtribes is a SaaS platform for real-time, data-driven UX targeting",
+        quantity: 51
+    },
+
+    "w11H5lBZP2PC5P6Hi9RzT": {
+        name: "DIVA",
+        description: "An advanced OTT player, synchronizing real-time data with rich interactivity.",
+        quantity: 15,
+    },
+
+    "SY4pBCLf65l4nl6E5m_E7":
+    {
+        name: "AXIS",
+        description: "A targeted UX management console and suite of multi-platform reference apps.",
+        quantity: 100
+    },
+
+    "JeC5pzjs8Nw2YZnV8ot1m":
+    {
+        name: "FORGE",
+        description: "The world's first sport-focused publishing platform.",
+        quantity: 1
+    },
+
+    "JeC5pqwveJw2YZnV8ot1m":
+    {
+        name: "Data Services",
+        description: "Our advanced, real-time data tracking solutions empower rights holders to maximize the value of their content",
+        quantity: 99
+    },
+
+    "JeC5pzewwr1wetewttt1m":
+    {
+        name: "Design Services",
+        description: "Our Design Services help sports operators clarify their vision, investigate user needs, design a powerful digital product, and iterate on the results",
+        quantity: 75
+    },
+
+    "qwetfdbfdbpojgaeggdgj":
+    {
+        name: "VISUAL STORIES",
+        description: "This flexible module enables operators to craft bite-sized messages pre, during, and post-game to make your digital experiences even more compelling.",
+        quantity: 44
+    },
+
+    "xvmn99hioeurwghbbfbff":
+    {
+        name: "LIVE BLOGGING",
+        description: "This flexible module enables operators to craft bite-sized messages pre, during, and post-game to make your digital experiences even more compelling.",
+        quantity: 10
+    },
+
+    "zcxbzcxbmnkjiCOLw28bw":
+    {
+        name: "mtribes",
+        description: "mtribes is a SaaS platform for real-time, data-driven UX targeting",
+        quantity: 51
+    },
 }
 
 module.exports = products
